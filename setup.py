d396aaa53a41ca62ca4258d64fb6b2a4c0e70d7d
Vinodh Rajan <vinodh@virtualvinodh.com>
2022-06-10 18:23:24 +0100

    new pypi version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
   # Versions should comply with PEP440.  For a discussion on single-sourcing
   # the version across setup.py and the project code, see
   # https://packaging.python.org/en/latest/single_source_version.html
-  version='2.0.2',
+  version='2.0.3',
 
   description='Provides script conversion (a.k.a transliteration) between various scripts',
   long_description=long_description,
