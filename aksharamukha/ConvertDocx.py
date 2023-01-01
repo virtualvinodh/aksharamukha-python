ab438845ebc42ffeaa34dfd3010a6cad8dc2b885
mhdsulaimantan <mohammad.sulaiman.tanbari@gmail.com>
2023-01-01 15:51:17 +0100

    Update functions structure

--- aksharamukha/ConvertDocx.py
+++ aksharamukha/ConvertDocx.py
@@ -6,6 +6,34 @@ from zipfile import ZipFile
 MS_WORD_TEXT_SCHEMA = "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}t"
 
 
+def convert_docx(src, tgt, file, nativize=True, pre_options=[], post_options=[], is_api_mode=False):
+    conversion_data = {
+        'source': src,
+        'target': tgt,
+        'nativize': nativize,
+        'pre_options': pre_options,
+        'post_options': post_options
+    }
+
+    new_zip = io.BytesIO()
+
+    with ZipFile(file, 'r') as old_archive:
+        with ZipFile(new_zip, 'w') as new_archive:
+            parse_content(conversion_data, old_archive, new_archive)
+
+    file = flush_zip_to_file(file, new_zip, is_api_mode)
+
+    return file
+
+
+def parse_content(data, old_archive, new_archive):
+    for item in old_archive.filelist:
+        if item.filename.startswith('word/') and item.filename.endswith('.xml'):
+            xml_file_to_convert(data, old_archive, new_archive, item)
+        else:
+            new_archive.writestr(item, old_archive.read(item.filename))
+
+
 def xml_file_to_convert(data, old_archive, new_archive, item):
     with old_archive.open(item, 'r') as xml_file:
         tree = etree.parse(xml_file)
@@ -26,16 +54,8 @@ def xml_file_to_convert(data, old_archive, new_archive, item):
         new_archive.writestr(item, string)
 
 
-def parse_content(data, old_archive, new_archive):
-    for item in old_archive.filelist:
-        if item.filename.startswith('word/') and item.filename.endswith('.xml'):
-            xml_file_to_convert(data, old_archive, new_archive, item)
-        else:
-            new_archive.writestr(item, old_archive.read(item.filename))
-
-
-def flush_zip_to_file(file, zip, api):
-    if api:
+def flush_zip_to_file(file, zip, is_api_mode):
+    if is_api_mode:
         # in-memory stream
         file = io.BytesIO(zip.getvalue())
         return file
@@ -43,23 +63,3 @@ def flush_zip_to_file(file, zip, api):
         # path
         with open(file, 'wb') as f:
             f.write(zip.getvalue())
-
-
-def convert_docx(src, tgt, file, nativize=True, pre_options=[], post_options=[], api=False):
-    conversion_data = {
-        'source': src,
-        'target': tgt,
-        'nativize': nativize,
-        'pre_options': pre_options,
-        'post_options': post_options
-    }
-
-    new_zip = io.BytesIO()
-
-    with ZipFile(file, 'r') as old_archive:
-        with ZipFile(new_zip, 'w') as new_archive:
-            parse_content(conversion_data, old_archive, new_archive)
-
-    file = flush_zip_to_file(file, new_zip, api)
-
-    return file
