e2ef353289115ad467b3be92d1ca41764bdbb966
Vinodh Rajan <vinodh@virtualvinodh.com>
2024-05-12 18:38:37 +0200

    fixing commands for the pip package

--- pythonbuild.ps1
+++ pythonbuild.ps1
@@ -2,11 +2,9 @@
 
 python setup.py sdist bdist_wheel
 
-python -m twine upload -u $args[1] -p $args[2] dist/*
-
-(Get-Content .\setup.py).replace( $args[0], '@@@') | Set-Content .\setup.py
+python -m twine upload -u __token__ -p replace_token_here dist/*
 
 python setup.py clean --all
 
 ### How to Run
-### ./pythonbuild.ps1 versionnumber pip-username pip-password
+### ./pythonbuild.ps1 versionnumber
